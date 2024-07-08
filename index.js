2b21adceeacdeb7d1c0e97fc2f2b520346602ec5
Slava <v4slava.guk@gmail.com>
2024-07-08 14:25:35 +0300

    clean

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+
+const pipedriveApi = require('./api');
+const pipedriveApiClient = pipedriveApi.apiClient
+
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 
-const config = require('./config');
+const PORT = 3000;
 
 app.use(cookieParser());
 app.use(cookieSession({
     name: 'session',
     keys: ['key1']
 }));
-const PORT = 3000;
-
-const pipedrive = require('pipedrive');
-
-const apiClient = new pipedrive.ApiClient();
-
-let oauth2 = apiClient.authentications.oauth2;
-oauth2.clientId = config.clientID || ''; // OAuth 2 Client ID
-oauth2.clientSecret = config.clientSecret || ''; // OAuth 2 Client Secret
-oauth2.redirectUri = config.callbackURL || ''; // OAuth 2 Redirection endpoint or Callback Uri
 
 app.disable('x-powered-by');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
 
 app.get('/', async (req, res) => {
     if (req.session.accessToken !== null && req.session.accessToken !== undefined) {
 
+        const oauth2 = pipedriveApiClient.authentications.oauth2
         if(!oauth2.accessToken){
             oauth2.accessToken = req.session.accessToken
             oauth2.refreshToken  = req.session.refreshToken
@@ -44,29 +33,32 @@ app.get('/', async (req, res) => {
         // token is already set in the session
         // now make API calls as required
         // client will automatically refresh the token when it expires and call the token update callback
-        const apiUser = new pipedrive.UsersApi(apiClient);
-        const currentUser = await apiUser.getCurrentUser();
+        const currentUserData = await pipedriveApi.getUserData()
 
         res.render('iframe', {
-			name: currentUser.data.name
+			name: currentUserData.name
 		});
     } else {
-        const authUrl = apiClient.buildAuthorizationUrl();;
+        const authUrl = pipedriveApiClient.buildAuthorizationUrl();;
 
         res.redirect(authUrl);
     }
 });
 
 app.get('/auth/pipedrive/callback', (req, res) => {
-    console.log(1)
     const authCode = req.query.code;
-    const promise = apiClient.authorize(authCode);
+    const promise = pipedriveApiClient.authorize(authCode);
 
     promise.then(() => {
-        req.session.accessToken = apiClient.authentications.oauth2.accessToken;
-        req.session.refreshToken = apiClient.authentications.oauth2.refreshToken;
+        const oauth2 = pipedriveApiClient.authentications.oauth2
+        req.session.accessToken = oauth2.accessToken;
+        req.session.refreshToken = oauth2.refreshToken;
         res.redirect('/');
     }, (exception) => {
         // error occurred, exception will be of type src/exceptions/OAuthProviderException
     });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
